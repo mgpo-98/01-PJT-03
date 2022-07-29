5cc1bd843b0c10f0d78a51a83d8b5f07d636454e
mgpo-98 <csi4613@naver.com>
2022-07-29 17:36:01 +0900

    조승윤 0729

--- 2회차/조승윤/3_직사각형길이찾기.py
+++ 2회차/조승윤/3_직사각형길이찾기.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_직사각형길이찾기.txt")
+t = int(input())
+for i in range(1,t+1):
+    d = 0
+    a, b, c = map(int, input().split())
+    if (a == b):
+        d =c
+    elif(a == c):
+        d =b
+    elif (b == c):
+        d = a
+    else :
+        d =a 
+    print(f'#{i} {d}')
